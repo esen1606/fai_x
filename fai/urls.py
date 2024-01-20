65972e864162e1a77b5fa1f0c50f7f55b997c6b6
Esen <esenkackynbaev6@gmail.com>
2024-01-20 19:17:19 +0600

    fai_x

--- fai/urls.py
+++ fai/urls.py
@@ -15,7 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,  re_path
+from rest_framework.routers import DefaultRouter
+from category.views import CategoryCreteListView
+from post.views import PostViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+router = DefaultRouter()
+router.register('categories', CategoryCreteListView),
+router.register('products', PostViewSet)
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Fai",
+        default_version='v1',
+        description="food at is it",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="__"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -25,4 +49,6 @@ urlpatterns = [
     path('category/', include('category.urls')),
     path('comment/', include('comment.urls')),
     path('like/', include('like.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
